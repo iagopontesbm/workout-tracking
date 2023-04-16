1296682d38fbccded43d649d26fde80b0a01e287
Iago <iago.pontes@outlook.com>
2023-04-16 17:38:25 -0300

    Comentários e documentação. Foi adicionado a váriavel SHEETY_ENDPOINT que pega o link do endpoint das variáveis do ambiente python.

--- main.py
+++ main.py
@@ -2,34 +2,41 @@ import requests
 import os
 from datetime import datetime
 
+# Variáveis do ambiente
 NUTRITIONIX_API_ID = os.getenv("NUTRITIONIX_API_ID")
 NUTRITIONIX_API_KEY = os.getenv("NUTRITIONIX_API_KEY")
 SHEET_BEARER_KEY = os.getenv("SHEET_BEARER_KEY")
+SHEETY_ENDPOINT = os.getenv("SHEETY_ENDPOINT")
 
+# Endipoints APIs
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheet_endpoint = "https://api.sheety.co/1c8881ec505e12e09cd78b4c264b7242/myWorkouts/workouts"
+sheet_endpoint = SHEETY_ENDPOINT
 
+# Header NUTRITIONIX API
 headers = {
     "x-app-id": NUTRITIONIX_API_ID,
     "x-app-key": NUTRITIONIX_API_KEY,
     "x-remote-user-id": "0"
 }
 
+exercise = input("Informe a atividade realizada: ")
+
+# Informação enviada para NUTRITIONIX
 exercise_data = {
-    "query": "swim 200 meters",
-    "gender": "male",
-    "weight_kg": 68,
-    "height_cm": 180,
-    "age": 30
+    "query": exercise,  # Atividade executada
+    "gender": "male",  # Gênero
+    "weight_kg": 68,  # Peso
+    "height_cm": 180,  # Altura
+    "age": 30  # Idade
 }
 
+# POST e retorno informações NUTRITIONIX
 response = requests.post(exercise_endpoint, exercise_data, headers=headers)
 response_data = response.json()["exercises"][0]
 
-print(response_data)
-
 today = datetime.today()
 
+# Informações enviada para SHEET
 sheet_data = {
     "workout": {
         "date": today.strftime("%d/%m/%Y"),
@@ -40,11 +47,10 @@ sheet_data = {
     }
 }
 
+# Header SHEET API -- https://stackoverflow.com/questions/29931671/making-an-api-call-in-python-with-an-api-that-requires-a-bearer-token
 sheet_headers = {
     "Authorization": f"Bearer {SHEET_BEARER_KEY}"
 }
-
-print(sheet_data)
 
 sheet_response = requests.post(url=sheet_endpoint, json=sheet_data, headers=sheet_headers)
 
